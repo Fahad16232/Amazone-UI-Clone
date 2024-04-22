8bc221d627d32aae1d7917846e496f8ba01ea704
Jahanzaib Abid <fahadabid304@gmail.com>
2024-04-22 16:06:45 +0500

    Initial Commit

---
+++ MVVM Project Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Project Demo
+//
+//  Created by Fahad on 22/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
